3725eb54d664b63208573a452ac2b2098a39f95e
Rafael Silva <130245184+rafs-m1ke@users.noreply.github.com>
2023-08-24 12:52:15 -0700

    ex final

--- oneBitCode/pythonFund/exercícioFinal.py
+++ oneBitCode/pythonFund/exercícioFinal.py
@@ -18,3 +18,81 @@ Escreva um programa em python que realize o gerenciamento de jogadores. Ele deve
 
 Este é o exercício de revisão do módulo, então aproveite para utilizar todos os recursos vistos até agora, como os funções, condições, loop, listas, etc.
 """
+teams = {}
+done = False
+
+# função para listar times
+def printTeams():
+    print("Times Listados: ")
+    for i, team in enumerate(teams.values()):
+        print(f"{i + 1}. {team['name']} ({len(team['players'])} jogadores)")
+
+# função para listar jogadores de um time
+def printTeamPlayers():
+    print(f"Jogadores do time {teams['name']}")
+    for i, player in enumerate(teams['players']):
+        print(f"{i+1}. {player}")
+    
+while not done:
+    # Opções no programa
+    print('''
+Oque deseja fazer?
+1. Adicionar um time
+2. Remover um time
+3. Listar um time
+4. Adicionar jogador em um time
+5. Remover jogador de uma time
+6. Listar jogadores de um time
+7. Sair
+          ''')
+    
+    choice = input("> ")
+    if choice == "1":
+        teamName = input("Digite o nome do time: ")
+        teams[teamName] = {"name" : teamName, "players" : []}
+    if choice == "2":
+        printTeams()
+        teamNum = int(input("Informe o número do time a ser removido: "))
+        if teamNum <= len(teams):
+            teamName = list(teams.keys())[teamNum - 1]
+            del teams[teamName]
+        else:
+            print("Número do time inválido!")
+    if choice == "3":
+        printTeams()
+    if choice == "4":
+        printTeams()
+        teamNum = int(input("Informe o numero do time onde deseja adicionar o jogador: "))
+        if teamNum <= len(teams):
+            teamName = list(teams.keys())[teamNum - 1]
+            playerName = input("Informe o nome do jogador: ")
+            teams[teamName]["players"].append(playerName)
+            print("Jogador adicionado no time")
+        else:
+            print("Número do time está inválido")
+    if choice == "5":
+        printTeams()
+        teamNum = int(input("Informe o número do time que deseja remover o jogador: "))
+        if teamNum <= len(teams):
+            teamName = list(teams.keys())[teamNum -1]
+            printTeamPlayers(teams[teamName])
+            playerNum = int(input("Informe o número do jogador que deseja remover: "))
+            if playerNum <= len(teams[teamName]['players']):
+                del teams[teamName]['players'][playerNum - 1]
+                print("Jogador removido do time")
+            else:
+                print("Número do jogador inválido!")
+        else:
+            print("Número do time inválido!")
+    if choice == "6":
+        printTeams()
+        teamNum = int(input("Informe o numero do time onde deseja listar o jogador: "))
+        if teamNum <= len(teams):
+            teamName = list(teams.keys())[teamNum - 1]
+            printTeamPlayers(teams[teamName])
+        else:
+            print("Numero do time inválido")
+    if choice == "7":
+        done = True
+    else:
+        print("Opção inválida")
