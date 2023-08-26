b724756bb87af1c10d72c88ab6ae2fd2bcbf26b5
Rafael Silva <130245184+rafs-m1ke@users.noreply.github.com>
2023-08-25 23:43:52 -0700

    ex1

---
+++ oneBitCode/modulos-pip/ex1.py
@@ -0,0 +1,13 @@
+def inverse(string):
+    contrarian = string[::-1]
+    return contrarian
+
+
+def oddIndex(index):
+    odd = index[0::2]
+    return odd
+
+
+def pairIndex(index):
+    pair = index[1::2]
+    return pair
--- oneBitCode/modulos-pip/teste-ex1.py
+++ oneBitCode/modulos-pip/teste-ex1.py
@@ -1,1 +1,5 @@
-import modulo-ex1
+import ex1
+
+print(ex1.inverse("Rafael"))
+print(ex1.oddIndex("Rafael"))
+print(ex1.pairIndex("Rafael"))
