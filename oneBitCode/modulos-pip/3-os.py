d9bdceb00619a8dfe6fb7da045f6e285b685484a
Rafael Silva <130245184+rafs-m1ke@users.noreply.github.com>
2023-08-26 00:27:02 -0700

    att

---
+++ oneBitCode/modulos-pip/3-os.py
@@ -0,0 +1,19 @@
+import os
+
+# 1- Consultar funcionalidades do módulo OS
+# help(os)
+
+# 2- Retornar a pasta atual
+print(os.getcwd())
+
+# 3- Listar arquivos e pastas
+print(os.listdir())
+
+# 4- Apresentar a versão do SO
+os.system('ls')
+
+# 5- Configurações da máquina
+os.system('systeminfo')
+
+# 6- Mostrar o IP
+os.system("ifconfig")
